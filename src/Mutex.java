1ea33ca19286484dde483bde5f3dc4555be3bf1f
Aleksandr <56514648+samedovAV@users.noreply.github.com>
2020-04-12 22:21:18 +0300

    Mutex

--- src/Mutex.java
+++ src/Mutex.java
@@ -1,116 +1,137 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Random;
+import java.io.IOException;
 import java.util.concurrent.Semaphore;
 
 public class Mutex {
 
-    public static void main(String[] args) {
-        // userChoice();
-        // Создает новые потоки когда выбираю повторить...
-        createThreads();
-        userChoice();
-
-
-
-        /*
-        oneThread thread1 = new oneThread();
-        oneThread thread2 = new oneThread();
-        oneThread thread3 = new oneThread();
-        oneThread thread4 = new oneThread();
-        oneThread thread5 = new oneThread();
-        */
-
-        /*
-        thread1.place = place;
-        thread2.place = place;
-        thread3.place = place;
-        thread4.place = place;
-        thread5.place = place;
-        */
-        /*
-        thread1.start();
-        thread2.start();
-        thread3.start();
-        thread4.start();
-        thread5.start();
-        */
-
+    public static void showMenu() {
+        System.out.println("Введите: ");
+        System.out.println("1. Чтобы продолжить работу");
+        System.out.println("2. Чтобы завершить работу");
     }
 
-    // Выбор дальнейших действий
-    public static void userChoice() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Повторить - 0; Выйти - 1");
-        int choice = input.nextInt();
-        if (choice == 0) {
-            createThreads();
-            userChoice();
+    public static void main(String[] args) throws InterruptedException, IOException{
+        System.out.println("Main thread started");
+        BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
+        int choice;
+        // Создание потоков
+        oneThread[] threads = new oneThread[]{
+                new oneThread("thread1"),
+                new oneThread("thread2"),
+                new oneThread("thread3"),
+                new oneThread("thread4"),
+                new oneThread("thread5")
+        };
+
+        // Запуск потоков
+        for (oneThread item : threads) {
+            item.start();
         }
-        else if (choice == 1) {
-            System.out.println("Работа с потоками закончена.");
-        }
-        else
-        {
-            System.out.println("Неккоректный ввод. Повторите.\n");
-            userChoice();
-        }
-    }
-
-    public static void createThreads() {
-
-        oneThread thread1 = new oneThread("Thread 1");
-        thread1.start();
-        oneThread thread2 = new oneThread("Thread 2");
-        thread2.start();
-        oneThread thread3 = new oneThread("Thread 3");
-        thread3.start();
-        oneThread thread4 = new oneThread("Thread 4");
-        thread4.start();
-        oneThread thread5 = new oneThread("Thread 5");
-        thread5.start();
-
-        try {
-            thread1.join();
-            thread2.join();
-            thread3.join();
-            thread4.join();
-            thread5.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    static Semaphore place = new Semaphore(1);
-
-    static class oneThread extends Thread {
-        String threadName;
-
-        oneThread(String threadName) {
-            this.threadName = threadName;
-        }
-
-        @Override
-        public void run() {
-            Random rand = new Random();
-            int randomNum = rand.nextInt((1000 - 100) + 1) + 10;
-            System.out.println(this.getName() + " waiting to run");
-
-            try {
-                place.acquire();
-                try {
-
-                    this.sleep(randomNum);
-                } finally {
-                    place.release();
-
+        do {
+            for(oneThread thread : threads) {
+                while (thread.getflowThread().getState() != Thread.State.WAITING) {
+                    Thread.sleep(1);
                 }
-            } catch(InterruptedException e) {
-                e.printStackTrace();
             }
-            System.out.println(this.getName() + " is running");
-            System.out.println(this.getName() + " worked " + randomNum);
+            showMenu();
+            choice = Integer.parseInt(buff.readLine());
+
+            switch (choice) {
+                case 1:
+                    for (oneThread item : threads) {
+                        synchronized (item.getLock()) {
+                            item.notWait();
+                        }
+                    }
+                    break;
+                case 2:
+                    for (oneThread item : threads) {
+                        synchronized (item.getLock()) {
+                            item.stop();
+                            item.notWait();
+                        }
+                    }
+            }
+        } while (choice != 2);
+        for (oneThread item : threads) {
+            item.getflowThread().join();
         }
+        System.out.println("Main thread finished");
     }
 }
 
+class oneThread implements Runnable {
+    // Подлючение Mutex (В Java - Semaphore)
+    private final Semaphore lock;
 
+    Random rand = new Random();
+    int randomNum = rand.nextInt((1000 - 100) + 1) + 10;
+
+    public Thread thread;
+    private String threadName;
+    private boolean isActive;
+
+    // Конструктор потока
+    oneThread(String name) {
+        threadName = name;
+        isActive = true;
+        lock = new Semaphore(1); // Mutex - это Semaphore с кол-вом разрешений 1
+    }
+
+    @Override
+    public void run() {
+        while (isActive) {
+            try {
+                lock.acquire();
+                int randomNum = rand.nextInt((1000 - 100) + 1) + 10;
+                try {
+                    Thread.sleep(randomNum);
+                    System.out.println(threadName + " woke up after " + randomNum + " ms");
+                }
+                catch (InterruptedException e) {
+                    System.out.println("Thread has been interrupted");
+                }
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        int randomNum = rand.nextInt((1000 - 100) + 1) + 10;
+        try {
+            Thread.sleep(randomNum);
+        }
+        catch (InterruptedException e) {
+            System.out.println("Thread has been interrupted");
+        }
+        System.out.println("Thread " + threadName + " is stopped and worked " + randomNum + " ms");
+    }
+
+
+    public void start() {
+        System.out.println("Thread " + threadName + " is running");
+        if (thread == null) {
+            thread = new Thread(this, threadName);
+            thread.start();
+        }
+    }
+
+    // Остановка потока
+    public void stop() {
+        this.isActive = false;
+    }
+
+    public Thread getflowThread() {
+        return thread;
+    }
+
+    public Object getLock() {
+        return lock;
+    }
+
+    public void notWait() {
+        this.lock.release();
+    }
+}
